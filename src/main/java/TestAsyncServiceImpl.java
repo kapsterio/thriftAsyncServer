9c1eabdd639af5f044aed445310b40742ef1d97f
zhangheng09 <zhangheng09@meituan.com>
2016-10-23 20:37:07 +0800

    size为计算md5

--- src/main/java/TestAsyncServiceImpl.java
+++ src/main/java/TestAsyncServiceImpl.java
@@ -1,12 +1,12 @@
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.concurrent.FutureCallback;
 import org.apache.http.impl.nio.client.CloseableHttpAsyncClient;
 import org.apache.thrift.TException;
 import org.apache.thrift.async.AsyncMethodCallback;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.security.MessageDigest;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -20,50 +20,31 @@ public class TestAsyncServiceImpl implements TestAsync.AsyncIface {
     AtomicInteger sucessCount = new AtomicInteger(0);
     ExecutorService executorService;
 
-    public TestAsyncServiceImpl(CloseableHttpAsyncClient client, ExecutorService executorService) {
+    public TestAsyncServiceImpl() {
+
+    }
+
+    public TestAsyncServiceImpl(CloseableHttpAsyncClient client, ExecutorService executorService) throws  Exception{
         //httpclient = HttpAsyncClients.createDefault();
         this.httpclient = client;
         this.httpclient.start();
         this.executorService = executorService;
     }
+
     @Override
-    public void size(AsyncMethodCallback resultHandler) throws TException {
+    public void size(AsyncMethodCallback resultHandler) throws TException{
+
         try {
-            // One most likely would want to use a callback for operation result
-
-            final HttpGet request2 = new HttpGet("http://open-in.meituan.com/");
-            long begin = System.currentTimeMillis();
-            //LOGGER.info("begin: {}", begin);
-            httpclient.execute(request2, new FutureCallback<HttpResponse>() {
-
-                public void completed(final HttpResponse response2) {
-                    /*executorService.execute(() -> {
-                        long end = System.currentTimeMillis();
-                        LOGGER.info("completed: {}, take: {}", end, end - begin);
-                        sucessCount.incrementAndGet();
-                        resultHandler.onComplete(response2.getStatusLine().getStatusCode());
-                    });*/
-                    LOGGER.info("take: {}", System.currentTimeMillis() - begin);
-                    resultHandler.onComplete(response2.getStatusLine().getStatusCode());
-
-                }
-
-                public void failed(final Exception ex) {
-                        LOGGER.info("take: {}", System.currentTimeMillis() - begin);
-                        errorCount.incrementAndGet();
-                        resultHandler.onComplete(-1);
-                        LOGGER.error("wrong: ", ex);
-                }
-
-                public void cancelled() {
-                    System.out.println(request2.getRequestLine() + " cancelled");
-                }
-
-            });
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update("hello".getBytes());
+            ByteBuffer bb = ByteBuffer.wrap(md.digest());
+            bb.order(ByteOrder.LITTLE_ENDIAN);
+            resultHandler.onComplete(bb.getInt());
         } catch (Exception e) {
-            errorCount.incrementAndGet();
-            //System.out.println(e);
             e.printStackTrace();
+            resultHandler.onComplete(-1);
         }
     }
+
+
 }
--- src/main/java/TestServiceImpl.java
+++ src/main/java/TestServiceImpl.java
@@ -1,10 +1,9 @@
-import org.apache.http.HttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.concurrent.FutureCallback;
 import org.apache.http.impl.nio.client.CloseableHttpAsyncClient;
 import org.apache.thrift.TException;
 
-import java.util.concurrent.CompletableFuture;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.security.MessageDigest;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /*
@@ -14,56 +13,27 @@ public class TestServiceImpl implements TestAsync.Iface {
     CloseableHttpAsyncClient httpclient;
     AtomicInteger errorCount = new AtomicInteger();
     AtomicInteger sucessCount = new AtomicInteger();
+    //MessageDigest md;
 
-    public TestServiceImpl(CloseableHttpAsyncClient client) {
+    public TestServiceImpl(CloseableHttpAsyncClient client) throws Exception{
         //httpclient = HttpAsyncClients.createDefault();
         this.httpclient = client;
         this.httpclient.start();
+      //  this.md = MessageDigest.getInstance("MD5");
     }
 
 
     @Override
     public int size() throws TException {
-
         try {
-            // One most likely would want to use a callback for operation result
-            final HttpGet request2 = new HttpGet("http://open-in.meituan.com/");
-
-            //final List<HttpResponse> result = new ArrayList<>();
-            CompletableFuture<Integer> result = new CompletableFuture<>();
-            long begin = System.currentTimeMillis();
-            httpclient.execute(request2, new FutureCallback<HttpResponse>() {
-
-                public void completed(final HttpResponse response2) {
-                    sucessCount.incrementAndGet();
-                    //response = response2;
-                    //result.add(response2);
-                    result.complete(response2.getStatusLine().getStatusCode());
-                    //System.out.println(request2.getRequestLine() + "->" + response2.getStatusLine());
-                }
-
-                public void failed(final Exception ex) {
-                    errorCount.incrementAndGet();
-
-                    result.complete(-1);
-                    System.out.println(request2.getRequestLine() + "->" + ex);
-                }
-
-                public void cancelled() {
-                    System.out.println(request2.getRequestLine() + " cancelled");
-                }
-
-            });
-            //latch1.await();
-            Integer response = result.get();
-            System.out.println("time :" + (System.currentTimeMillis()- begin));
-            return response;
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            md.update("hello".getBytes());
+            ByteBuffer bb = ByteBuffer.wrap(md.digest());
+            bb.order(ByteOrder.LITTLE_ENDIAN);
+            return bb.getInt();
         } catch (Exception e) {
-            errorCount.incrementAndGet();
-            System.out.println(e);
-            //e.printStackTrace();
-
-            return 0;
+            e.printStackTrace();
+            return -1;
         }
 
     }
